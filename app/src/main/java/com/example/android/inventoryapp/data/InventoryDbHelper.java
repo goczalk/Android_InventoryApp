ee4329d61ddb3a39a9811be70665754f01084928
Klaudia Goczal <klaudia@lodz.pl>
2018-08-09 12:11:20 +0200

    Added DbHelper

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,38 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.inventoryapp.data.InventoryContract.ProductEntry;
+
+/**
+ * Created by klaudia on 09/08/18.
+ */
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "inventory.db";
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+                + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER DEFAULT 0, "
+                + ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME + " TEXT, "
+                + ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE + " TEXT);";
+
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
