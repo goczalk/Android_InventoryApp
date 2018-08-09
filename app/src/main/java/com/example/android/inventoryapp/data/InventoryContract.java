70539580c54cc3f7464da39686c1aeaa34654d76
Klaudia Goczal <klaudia@lodz.pl>
2018-08-09 12:04:32 +0200

    Added contract and id for textview

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,22 @@
+package com.example.android.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by klaudia on 09/08/18.
+ */
+
+public final class InventoryContract {
+    private InventoryContract() {
+    }
+
+    public static class ProductEntry implements BaseColumns{
+        public static final String TABLE_NAME = "products";
+        public static final String _ID = "_id";
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+        public static final String COLUMN_PRODUCT_SUPPLIER_PHONE = "supplier_phone";
+    }
+}
