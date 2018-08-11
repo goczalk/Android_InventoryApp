147d7c9ac8cf3f87e993e2a282f8fc1184836c1b
Klaudia Goczal <klaudia@lodz.pl>
2018-08-11 18:45:33 +0200

    Added layouts for list view

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -7,17 +7,17 @@ import android.provider.BaseColumns;
  */
 
 public final class InventoryContract {
-    private InventoryContract() {
-        throw new AssertionError("No InventoryContractinstances for you!");
-    }
+  private InventoryContract() {
+    throw new AssertionError("No InventoryContractinstances for you!");
+  }
 
-    public static class ProductEntry implements BaseColumns{
-        public static final String TABLE_NAME = "products";
-        public static final String _ID = "_id";
-        public static final String COLUMN_PRODUCT_NAME = "name";
-        public static final String COLUMN_PRODUCT_PRICE = "price";
-        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
-        public static final String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
-        public static final String COLUMN_PRODUCT_SUPPLIER_PHONE = "supplier_phone";
-    }
+  public static class ProductEntry implements BaseColumns{
+    public static final String TABLE_NAME = "products";
+    public static final String _ID = "_id";
+    public static final String COLUMN_PRODUCT_NAME = "name";
+    public static final String COLUMN_PRODUCT_PRICE = "price";
+    public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+    public static final String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+    public static final String COLUMN_PRODUCT_SUPPLIER_PHONE = "supplier_phone";
+  }
 }
